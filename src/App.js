5ce71638e02deddf7c7de992cd7c0e7d34d9969a
akr9368 <rajput.avnesh0123@gmail.com>
2023-02-14 21:11:57 +0530

    search result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "react-bootstrap";
+import RedBusNavbar from "./components/Navbar";
+import Search from "./components/Search";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchResults from "./components/SearchResults";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Toaster />
+        <RedBusNavbar />
+        <Search />
+        {/* Show these three things everywhere but after this the content should be shown according to the routes */}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results" element={<SearchResults />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
