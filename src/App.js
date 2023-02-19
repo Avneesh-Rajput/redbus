5913565cc623a63aed4515e001502299cb0ac4d1
akr9368 <rajput.avnesh0123@gmail.com>
2023-02-19 11:44:35 +0530

    complete redbus clone

--- src/App.js
+++ src/App.js
@@ -6,21 +6,37 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchResults from "./components/SearchResults";
 import Home from "./components/Home";
+import { useState } from "react";
+import JourneyContext from "./context/JourneyContext";
+import SeatSelection from "./components/SeatSelection";
+import Journey from "./components/Journey";
+import Login from "./components/Login";
 
 function App() {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
   return (
     <div className="App">
-      <BrowserRouter>
-        <Toaster />
-        <RedBusNavbar />
-        <Search />
-        {/* Show these three things everywhere but after this the content should be shown according to the routes */}
+      <JourneyContext.Provider
+        value={{
+          from: from,
+          to: to,
+          setFrom: setFrom,
+          setTo: setTo,
+        }}
+      >
+        <BrowserRouter>
+        
+          {/* Show these three things everywhere but after this the content should be shown according to the routes */}
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/results" element={<SearchResults />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Journey Component={Home} />} />
+            <Route path="/results" element={<Journey Component={SearchResults}/>} />
+            <Route path="/book-seats" element={<Journey Component={SeatSelection} />} />
+          </Routes>
+        </BrowserRouter>
+      </JourneyContext.Provider>
     </div>
   );
 }
